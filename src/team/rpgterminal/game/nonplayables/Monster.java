8863c2c001fb9b7fa2be05e90b916d3b96fc66d3
NelsonSilva <njsilva.film@gmail.com>
2017-07-10 20:37:59 +0100

    Monsters added to UML. UML ready

---
+++ src/team/rpgterminal/game/nonplayables/Monster.java
@@ -0,0 +1,27 @@
+package team.rpgterminal.game.nonplayables;
+
+/**
+ * Created by codecadet on 10/07/2017.
+ */
+public class Monster extends Enemy {
+
+    @Override
+    public int hits() {
+        return super.hits();
+    }
+
+    @Override
+    public void attack() {
+        super.attack();
+    }
+
+    @Override
+    public void defend() {
+        super.defend();
+    }
+
+    @Override
+    public boolean isDestroyed() {
+        return super.isDestroyed();
+    }
+}
--- src/team/rpgterminal/game/nonplayables/NonPlayableCharacter.java
+++ src/team/rpgterminal/game/nonplayables/NonPlayableCharacter.java
@@ -3,5 +3,14 @@ package team.rpgterminal.game.nonplayables;
 /**
  * Created by codecadet on 10/07/2017.
  */
-public class NonPlayableCharacter {
+public class NonPlayableCharacter implements Destructible{
+    @Override
+    public int hits() {
+        return 0;
+    }
+
+    @Override
+    public boolean isDestroyed() {
+        return false;
+    }
 }
